8c8232cb0a52a285bb94b31e680db9085fabb99d
Matteo Giaimo <mgiaimo@hawk.iit.edu>
2018-06-06 10:36:58 -0500

    added icon for map
    added icon for ping tom park, it needs to be resized to better fit the map

--- mapjs.js
+++ mapjs.js
@@ -1,5 +1,7 @@
 var map;
 
+var pingtom;
+
 
         function initMap() {
         // Create a map object and specify the DOM element for display.
@@ -10,7 +12,13 @@ var map;
         mapTypeId: 'hybrid',
         });
 
-        
+         var pingtomMarker = new google.maps.Marker ({
+            position: {lat: 41.857297, lng: -87.634385},
+            map: map,
+            title: 'Ping Tom Memorial Park',
+            icon : 'Images/pinghut.svg',
+            animation: google.maps.Animation.DROP,
+			});
 
 
         }
